171aa15071110dc36e02659815b5172b94c0ab5e
Shahar Mayzel <shaharma@wix.com>
2025-03-20 14:01:41 +0200

    revove redundant code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from "react";
-import { useCounter } from "./context/CounterContext";
 import ProductList from "./components/ProductList";
 
 function App() {
-  const { count, increment, decrement, reset } = useCounter();
 
   return (
-    // <div style={{ textAlign: "center", marginTop: "50px" }}>
-    //   <h1>Count: {count}</h1>
-    //   <button onClick={increment}>Increment</button>
-    //   <button onClick={decrement}>Decrement</button>
-    //   <button onClick={reset}>Reset</button>
-    // </div>
     <>
     <ProductList/>
     </>
